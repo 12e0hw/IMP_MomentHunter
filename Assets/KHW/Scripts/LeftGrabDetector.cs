da220ad3a071ba1f79608e7b34acf5b14b5665e7
12e0hw <12e0hw@gmail.com>
2025-07-28 00:08:18 +0900

    [Feat] 튜토리얼 FSM 변경

--- Assets/KHW/Scripts/LeftGrabDetector.cs
+++ Assets/KHW/Scripts/LeftGrabDetector.cs
@@ -1,23 +1,48 @@
+// using UnityEngine;
+
+// public class LeftGrabDetector : MonoBehaviour
+// {
+//     public GameObject targetObject;      // Target object to detect
+//     public bool oneShot = true;          // Detect only once if true
+//     Collider _zoneCollider;
+
+//     void Awake()
+//     {
+//         _zoneCollider = GetComponent<Collider>();
+//         _zoneCollider.isTrigger = true;   // Set collider as trigger
+//     }
+
+//     void OnTriggerEnter(Collider other)
+//     {
+//         if (other.gameObject != targetObject) return;   // Only detect target object
+
+//         TutorialManager.Instance?.OnLeftGrabDone();    // Notify tutorial progress
+
+//         if (oneShot) enabled = false;     // Disable after first detection
+//     }
+// }
+
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class LeftGrabDetector : MonoBehaviour
 {
-    public GameObject targetObject;      // Target object to detect
-    public bool oneShot = true;          // Detect only once if true
-    Collider _zoneCollider;
+    [SerializeField] XRBaseInteractor leftHandInteractor;
+    bool isEnabled = false;
 
-    void Awake()
+    public void EnableDetector(bool enable)
     {
-        _zoneCollider = GetComponent<Collider>();
-        _zoneCollider.isTrigger = true;   // Set collider as trigger
+        isEnabled = enable;
+        if (leftHandInteractor == null) return;
+
+        if (enable)  leftHandInteractor.selectEntered.AddListener(OnGrab);
+        else         leftHandInteractor.selectEntered.RemoveListener(OnGrab);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnGrab(SelectEnterEventArgs _)
     {
-        if (other.gameObject != targetObject) return;   // Only detect target object
-
-        TutorialManager.Instance?.OnLeftGrabDone();    // Notify tutorial progress
-
-        if (oneShot) enabled = false;     // Disable after first detection
+        if (!isEnabled) return;
+        TutorialManager.Instance?.OnLeftGrabDone();
     }
 }
