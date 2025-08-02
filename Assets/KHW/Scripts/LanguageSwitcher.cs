c460ea25a812e33e9cc6aaff35bee9ab863a5c90
12e0hw <12e0hw@gmail.com>
2025-08-02 21:31:30 +0900

    [Add] 오프닝 다국어 설정 변환 추가

--- Assets/KHW/Scripts/LanguageSwitcher.cs
+++ Assets/KHW/Scripts/LanguageSwitcher.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
 
-public class UILanguageSwitcher : MonoBehaviour
+public class LanguageSwitcher : MonoBehaviour
 {
     public GameObject[] koreanUI; // 한글 UI 묶음
     public GameObject[] englishUI; // 영어 UI 묶음
 
-    void Start()
+    public void ApplyLanguage(int lang)
     {
-        int lang = PlayerPrefs.GetInt("language", 0); // 기본: 한국어
-
         bool isEnglish = (lang == 1);
 
         foreach (GameObject obj in koreanUI)
-            obj.SetActive(!isEnglish);
+            if (obj != null) obj.SetActive(!isEnglish);
 
         foreach (GameObject obj in englishUI)
-            obj.SetActive(isEnglish);
+            if (obj != null) obj.SetActive(isEnglish);
     }
+
+    void Start()
+    {
+        int lang = PlayerPrefs.GetInt("language", 0);
+        ApplyLanguage(lang); 
+    }
+
+
 }
