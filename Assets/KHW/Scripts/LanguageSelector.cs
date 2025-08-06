127d097821d6ff0fd2b0c8c57e08bd64cd9cc112
12e0hw <12e0hw@gmail.com>
2025-08-06 17:11:49 +0900

    [Chore] 오프닝 다국어 확인

--- Assets/KHW/Scripts/LanguageSelector.cs
+++ Assets/KHW/Scripts/LanguageSelector.cs
@@ -1,21 +1,25 @@
+
 using UnityEngine;
 
 public class LanguageSelector : MonoBehaviour
 {
-
     public LanguageSwitcher switcher;
+
     public void SetKorean()
     {
+        Debug.Log("SetKorean() 호출");
         PlayerPrefs.SetInt("language", 0); // 0 = 한국어
         PlayerPrefs.Save();
-        switcher.ApplyLanguage(0); //
+
+        switcher.ApplyLanguage(0);
     }
 
     public void SetEnglish()
     {
+        Debug.Log("SetEnglish() 호출");
         PlayerPrefs.SetInt("language", 1); // 1 = 영어
         PlayerPrefs.Save();
-        switcher.ApplyLanguage(0); //
+
+        switcher.ApplyLanguage(1);
     }
 }
-
